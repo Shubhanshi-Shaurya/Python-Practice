a76cd8a8f489ec30ce83cf6c1d67d8fcf78eef65
Shubhanshi Shaurya <shubhanshishaurya2006@gmail.com>
2026-05-27 17:13:40 +0530

    flask practice

---
+++ flask/app.py
@@ -0,0 +1,65 @@
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///user.db"
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(200),nullable=False)
+    password=db.Column(db.String(200),nullable=False)
+    datetime=db.Column(db.DateTime,default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    all_users=User.query.all()
+    return render_template('index.html',all_users=all_users)
+
+
+@app.route('/about')
+def about():
+    return "about page"
+
+
+@app.route('/register',methods=["GET",'POST'])
+def register():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        new_user=User(
+            username=username,
+            password=password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/')
+    return render_template('register.html')
+
+@app.route('/update/<sno>',methods=["GET",'POST'])
+def update(sno):
+    user=User.query.filter_by(sno=sno).first()
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        user.username=username
+        user.password=password
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html',user=user)
+
+@app.route('/delete/<sno>',methods=["GET",'POST'])
+def delete(sno):
+    user=User.query.filter_by(sno=sno).first()
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
